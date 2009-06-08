d6054496e3e58bdb6c400a1a5dc3fde09f2bbcfb
abet <abet@abet-desktop>
2009-06-08 15:09:24 +0900

    add instruction in README

--- xcrypt/UI.pm
+++ xcrypt/UI.pm
@@ -4,7 +4,7 @@ use File::Copy;
 use function;
 
 use base qw(Exporter);
-@EXPORT = qw(killall pickup prepare_submit_sync prepare_submit submit_sync prepare submit kaishu sync);
+@EXPORT = qw(killall pickup prepare_submit_sync prepare_submit submit_sync prepare submit repickup sync);
 
 my @allmembers = ('exe', 'arg1', 'arg2', 'ifile', 'ofile', 'oclmn', 'odlmtr', 'queue', 'option', 'stdofile', 'stdefile', 'proc', 'cpu');
 
@@ -175,37 +175,15 @@ sub sync {
 }
 
 sub repickup {
-    my @objs = &prepare(@_);
-    my @stdouts = ();
-    foreach (@objs) {
-	my $stdofile = File::Spec->catfile($_->{id}, 'stdout');
-	if ($_->{stdofile}) { $stdofile = $_->{stdofile}; }
-	my @stdlist = &pickup($stdofile, ',');
-	push (@stdouts, $stdlist[0]);
-    }
-    return @stdouts;
-}
-
-sub kaishu {
-    my %jg_rng_amp = @_;
-    my $id = $jg_rng_amp{'id'};
-    my @outputs;
-    my @arg1s;
-    foreach (@{$jg_rng_amp{'range1'}}) {
-	my $arg1;
-	if ($jg_rng_amp{'amp1'} eq '') {
-	    $arg1 = &identity($_);
-	} else {
-	    $arg1 = &{$jg_rng_amp{'amp1'}}($_);
+    my @outputs = ();
+    foreach (@_) {
+	if ($_->{ofile}) {
+	    my @stdlist = &pickup(File::Spec->catfile($_->{id}, $_->{ofile}),
+				  $_->{odlmtr});
+	    push (@stdouts, $stdlist[$self->{oclmn}]);
 	}
-	push (@arg1s, $arg1);
+	return @stdouts;
     }
-    foreach (@arg1s) {
-	my $outputfile = File::Spec->catfile($id . '_' . $_, $jg_rng_amp{'ofile'});
-	my @list = &pickup($outputfile, $jg_rng_amp{'odlmtr'});
-	push (@outputs , $list[$jg_rng_amp{'oclmn'}]);
-    }
-    return @outputs;
 }
 
 1;
