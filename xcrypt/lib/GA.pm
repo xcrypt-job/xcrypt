c659dd48000d4f491b848f6a9000a1e04e16d851
abet <abet@abet-desktop>
2009-08-18 21:38:40 +0900

    refine GA.pm along a new policy of before, start, and after

--- xcrypt/lib/GA.pm
+++ xcrypt/lib/GA.pm
@@ -2,11 +2,18 @@ package GA;
 
 use strict;
 use NEXT;
+use jobsched;
+use Recursive qw(fcopy dircopy rcopy);
+use File::Spec;
+use File::Path;
+use UI;
+use function;
+use Data_Generation;
 
 sub new {
     my $class = shift;
     my $self = $class->NEXT::new(@_);
-    $self->{exe} = $self->{exe} . ' ' . $self->{GA_count}
+    $self->{exe} = $ENV{XCRYPT} . '/bin/GA ' . $self->{GA_count}
                                 . ' ' . $self->{GA_lengthOfStr}
                                 . ' ' . $self->{GA_howToCrossover}
                                 . ' ' . $self->{GA_howToSelect};
@@ -15,7 +22,31 @@ sub new {
 
 sub start {
     my $self = shift;
-    $self->NEXT::start();
+
+    # 前回doneになったジョブならとばす．
+    if ( &jobsched::get_job_status ($self->{id}) eq 'done') {
+        print "Skipping " . $self->{id} . " because already done.\n";
+    } else {
+        $self->{request_id} = &jobsched::qsub($self);
+        # 結果ファイルから結果を取得
+        &jobsched::wait_job_done($self->{id});
+
+        my $stdofile = 'stdout';
+        unless ($self->{stdofile} eq '') { $stdofile = $self->{stdofile}; }
+
+	# NFSの書込み遅延に対する暫定的対応
+	sleep(3);
+
+        my $pwdstdo = File::Spec->catfile($self->{id}, $stdofile);
+        until ( (-e $pwdstdo) or ($jobsched::job_status{$self->{id}} eq 'abort')  ) {
+	    sleep 1;
+	}
+
+	unless ($self->{stdodlmtr}) { $self->{stdodlmtr} = ','; }
+	unless ($self->{stdoclmn}) { $self->{stdoclmn} = '0'; }
+        my @stdlist = &pickup($pwdstdo, $self->{stdodlmtr});
+        $self->{stdout} = $stdlist[$self->{stdoclmn}];
+    }
 }
 
 sub before {
