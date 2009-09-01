b6c6affadbeb818c9b404b72fc927033aee8b261
abet <abet@abet-desktop>
2009-09-01 22:20:01 +0900

    contract two watch threads into one

--- xcrypt/lib/jobsched.pm
+++ xcrypt/lib/jobsched.pm
@@ -700,12 +700,57 @@ sub check_and_write_aborted {
 #     }
 # }
 
+sub getjobids {
+    open( JOBIDS, "< $_[0]" );
+    my %reqid_jobids = split(' ', <JOBIDS>);
+    my %count;
+    my @vals = values(%reqid_jobids);
+    @vals = grep(!$count{$_}++, @vals);
+    my @jobids = sort @vals;
+    close( JOBIDS );
+    return @jobids;
+}
+
+sub check_and_alert_elapsed {
+    my $reqidfile = File::Spec->catfile ('inv_watch', '.request_ids');
+    my @jobids = &getjobids($reqidfile);
+
+    my $sum = 0;
+    my %elapseds = ();
+    foreach my $i (@jobids) {
+	my $time_done_now = time();
+	my $inventoryfile = File::Spec->catfile ('inv_watch', "$i");
+	my $time_running;
+	open( INV, "$inventoryfile" );
+	while (<INV>) {
+	    if ($_ =~ /^time_running\:\s*([0-9]*)/) {
+		$time_running = $1;
+	    }
+	    if ($_ =~ /^time_done\:\s*([0-9]*)/) {
+		$time_done_now = $1;
+	    }
+	}
+	close( INV );
+	my $elapsed = $time_done_now - $time_running;
+	$sum = $sum + $elapsed;
+	$elapseds{"$i"} = $elapsed;
+    }
+    my $length =  @jobids;
+    my $average = $sum / $length;
+    foreach (@jobids) {
+	if ( $elapseds{$_} - $average > 10 ) {
+	    print STDERR "Warning: $_ takes more time than other jobs.\n";
+	}
+    }
+}
+
 sub invoke_abort_check {
     $abort_check_thread = threads->new( sub {
         while (1) {
             sleep 19;
             check_and_write_aborted();
             # print_all_job_status();
+	    check_and_alert_elapsed();
         }
     });
 }
