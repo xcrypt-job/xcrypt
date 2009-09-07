dba82945696e7a83c38457b8d9f60e44e2deb4bc
sogawa <sogawa@ais.sys.i.kyoto-u.ac.jp>
2009-09-07 14:38:40 +0900

    add DC lib and sample.

---
+++ xcrypt/lib/DC.pm
@@ -0,0 +1,61 @@
+package DC;
+
+use strict;
+use NEXT;
+use UI;
+
+
+sub new
+{
+	my $class = shift;
+	my $self = $class->NEXT::new(@_);
+	print "\tnew DC\n";
+	return bless $self, $class;
+}
+
+sub start
+{
+	my $self = shift;
+	print "\tstart DC\n";
+	$self->NEXT::start();
+}
+
+sub before
+{
+	my $self = shift;
+	print "\tbefore DC\n";
+	
+	#if( &{$self->{canDivideFunc}}($self) )
+	if( $self->{arg0} > 10 )
+	{
+		my @children = &{$self->{divideFunc}}($self);
+		my @jobs = ();
+		foreach my $child (@children)
+		{
+			push(@jobs, &UI::prepare(%{$child}));
+		}
+		my @results = &UI::submit_sync(@jobs);
+		&{$self->{mergeFunc}}($self->{id}."/".$self->{ofname}, @results);
+		$self->{exe} = "echo hoge";
+	}
+	$self->NEXT::before();
+}
+
+sub after
+{
+	my $self = shift;
+	print "\tafter DC\n";
+	$self->NEXT::after();
+}
+
+sub divide
+{
+	
+}
+
+sub merge
+{
+
+}
+
+1;
