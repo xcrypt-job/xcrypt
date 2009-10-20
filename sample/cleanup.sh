218606a16c9a80d78556bd5a5c571e0c64a8e713
abet <abet@abet-laptop>
2009-10-20 13:49:48 +0900

    modify Xcrypt_manual

--- sample/cleanup.sh
+++ sample/cleanup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo 1; find . -name "*.xcr.pl"| xargs rm
-echo 2; find . -name "invwrite-sock.log"| xargs rm
+echo 1; find . -name "*.xcr.pl"| xargs rm -f
+echo 2; find . -name "invwrite-sock.log"| xargs rm -f
 echo 3; find . -name "job[0-9]*"| xargs rm -rf
 echo 4; find . -name "job_lu*"| xargs rm -rf
 echo 5; find . -name 'job-xcrypt'| xargs rm -rf
