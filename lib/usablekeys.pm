adcf5a740f82f4adac99c6f9eb11606974e7acfc
abet <abet@abet-laptop>
2009-12-22 15:52:00 +0900

    implement tSGE.pm

--- lib/usablekeys.pm
+++ lib/usablekeys.pm
@@ -1,5 +1,5 @@
 package usablekeys;
 
-our @allkeys = ('exe', 'stdofile', 'stdefile', 'queue', 'proc', 'cpu', 'option', 'after', 'before');
+our @allkeys = ('exe', 'stdofile', 'stdefile', 'queue', 'proc', 'cpu', 'group', 'option', 'after', 'before');
 
 1;
