32bbea3bc77d31539dfb39756588e84f4fb8fda3
abet <abet@abet-laptop>
2009-12-22 15:52:19 +0900

    add tSGE.pm

---
+++ lib/config/tSGE.pm
@@ -0,0 +1,30 @@
+# Config file for SGE
+$jsconfig::jobsched_config{"tSGE"} = {
+    qsub_command => "/opt/sge/local/bin/qsub2",
+    qdel_command => "/opt/sge/local/bin/qdel",
+    qstat_command => "/opt/sge/bin/lx24-amd64/qstat",
+    jobscript_preamble => ['#% -S /bin/sh', '#% -cwd'],
+#    jobscript_queue => '#$-q ',
+    jobscript_stdout => '#% -o ',
+    jobscript_stderr => '#% -e ',
+    jobscript_cpu => '#% -H ',
+    jobscript_group => '#% -P ',
+    extract_req_id_from_qsub_output => sub {
+        my (@lines) = @_;
+        if ($lines[1] =~ /^\s*Your\s+job\s+([0-9]+)/ ) {
+            return $1;
+        } else {
+            return -1;
+        }
+    },
+    extract_req_ids_from_qstat_output => sub {
+        my (@lines) = @_;
+        my @ids = ();
+        foreach (@lines) {
+            if ($_ =~ /^\s*([0-9]+)/) {
+                push (@ids, $1);
+            }
+        }
+        return @ids;
+    }
+};
