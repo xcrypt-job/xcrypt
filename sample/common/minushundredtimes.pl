330429c89825898d003a9a8e7e65dc74e9209829
abet <abet@abet-laptop>
2010-01-04 06:05:32 +0900

    remove a bug of boundary at bisection in n_section_method.pm, and support strictly decreasing functions

--- sample/common/minushundredtimes.pl
+++ sample/common/minushundredtimes.pl
@@ -5,7 +5,7 @@ my @tmp;
 open(IN, "< $ARGV[0]");
 my $line = <IN>;
 @tmp = split(' ', $line);
-$value = 100 * $tmp[2];
+$value = (-100) * $tmp[2];
 open(OUT, "> output.dat");
 print OUT $value;
 close(OUT);
