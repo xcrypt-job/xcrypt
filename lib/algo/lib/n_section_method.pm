74b585731983cb68f435df0db8e4bcaef43fc193
abet <abet@abet-laptop>
2010-01-04 22:53:25 +0900

    implement save of extra job executions in n_section_method.pm

--- lib/algo/lib/n_section_method.pm
+++ lib/algo/lib/n_section_method.pm
@@ -1,26 +1,82 @@
 package n_section_method;
 
 use base qw(Exporter);
+use threads;
+use threads::shared;
 our @EXPORT = qw(n_section_method);
+
+our %result : shared;
+our %id : shared;
 
 my $infinity = (2 ** 31) + 1;
 sub n_section_method {
     my ($num, $lt_k, $lt, $rt_k, $rt, $epsilon, $fun) = @_;
-    my %values;
     my $pt_k;
     my $pt;
+    my %thrd;
+    my %joined;
     do {
+	%result = ();
 	$seg = ($rt_k - $lt_k) / $num;
-	my @thrds;
 	foreach (1..($num-1)) {
 	    $pt_k = $lt_k + ($_ * $seg);
-	    $thrds[$_] = threads->new( \&$fun, $pt_k );
+	    $result{"$pt_k"} = undef;
 	}
 	foreach (1..($num-1)) {
 	    $pt_k = $lt_k + ($_ * $seg);
-	    $values{"$pt_k"} = $thrds[$_]->join;
+	    $thrd{"$pt_k"} = threads->new( \&$fun, $pt_k );
 	}
-	($lt_k, $lt, $rt_k, $rt) = pair_near_zero($lt_k, $lt, $rt_k, $rt, %values);
+	my $flag = 0;
+	until ($flag == 1) {
+	    sleep(3);
+	    foreach my $k (keys(%result)) {
+		if ((defined $result{"$k"})
+		    && ($result{"$k"} != $infinity)
+		    && ($result{"$k"} != 0 - $infinity)) {
+		    foreach my $l (keys(%result)) {
+			if ((0 <= $result{"$k"}) &&
+			    ((($lt <= $rt) && ($k < $l))
+			     || (($rt <= $lt) && ($l < $k)))
+			    ) {
+			    if ($joined{"$l"} == 0) {
+				my $jobid = $id{"$l"};
+				if ($jobid) {
+				    qx/xcryptdel $jobid/;
+				    $thrd{"$l"}->detach;
+				    $joined{"$l"} = 1;
+				    $result{"$l"} = $infinity;
+				}
+			    }
+			} elsif (($result{"$k"} < 0) &&
+				 ((($lt <= $rt) && ($l < $k))
+				  || (($rt <= $lt) && ($k < $l)))
+			    ) {
+			    if ($joined{"$l"} == 0) {
+				my $jobid = $id{"$l"};
+				if ($jobid) {
+				    qx/xcryptdel $jobid/;
+				    $thrd{"$l"}->detach;
+				    $joined{"$l"} = 1;
+				    $result{"$l"} = 0 - $infinity;
+				}
+			    }
+			}
+		    }
+		    if ($joined{"$k"} == 0) {
+			$thrd{"$k"}->join;
+			$joined{"$k"} = 1;
+		    }
+		}
+	    }
+	    $flag = 1;
+	    foreach my $k (keys(%result)) {
+		if (defined $result{"$k"}) {
+		} else {
+		    $flag = 0 * $flag;
+		}
+	    }
+	}
+	($lt_k, $lt, $rt_k, $rt) = pair_near_zero($lt_k, $lt, $rt_k, $rt, %result);
 	if (abs($lt) < abs($rt)) {
 	    $pt_k = $lt_k;
 	    $pt = $lt;
