54898d8d2ba681aba0b75e958109f439b9002003
tasuku <tasuku@PC-6173>
2010-01-22 12:51:35 +0900

    Fixed bug in source-me.sh and cpan/do-install.sh

--- cpan/do-install.sh
+++ cpan/do-install.sh
@@ -1,18 +1,14 @@
 #!/bin/sh
+## source source-me.sh before executing this script
+
 if [ "x$XCRYPT" = "x" ]; then
   echo "Set environment variable XCRYPT."
   exit 99
 fi
-
-## source source-me.sh before executing this script
-# CPAN_BASE=$XCRYPT/lib/cpan
-# for i in usr usr/local; do
-#   for j in lib lib64; do
-#       for k in perl5 perl5/site_perl; do
-#           export PERL5LIB=$CPAN_BASE/$i/$j/$k:$PERL5LIB
-#       done
-#   done
-# done
+if [ "x$XCR_CPAN_BASE" = "x" ]; then
+  echo "Set environment variable XCR_CPAN_BASE."
+  exit 99
+fi
 
 LIBS="File-Copy-Recursive-0.38 EV-3.9 Event-1.13 AnyEvent-5.24 common-sense-3.0 Guard-1.021 Coro-5.21-without-conftest"
 
@@ -24,11 +20,11 @@ do
 done
 
 echo "Removing CPAN install directory."
-rm -rf $CPAN_BASE/usr
+rm -rf $XCR_CPAN_BASE/usr
 
 echo "Start installation."
 for i in $LIBS
 do
   echo ">>> installing $i <<<"
-  (cd $i && perl Makefile.PL && make DESTDIR=$CPAN_BASE install)
+  (cd $i && perl Makefile.PL && make DESTDIR=$XCR_CPAN_BASE install)
 done
