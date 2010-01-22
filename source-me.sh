5c3b4ff023bb43285b97e662d0d9f9d31bf09c76
tasuku <tasuku@PC-6173>
2010-01-22 12:52:29 +0900

    Fixed imcomplete fixing

--- source-me.sh
+++ source-me.sh
@@ -1,11 +1,11 @@
 export XCRYPT=/path/to/xcrypt
 export XCRJOBSCHED="sh"
 export PERL5LIB=$XCRYPT/lib:$PERL5LIB
-CPAN_BASE=$XCRYPT/lib/cpan
+export XCR_CPAN_BASE=$XCRYPT/lib/cpan
 for i in usr usr/local; do
   for j in lib lib64; do
       for k in perl5 perl5/site_perl; do
-          export PERL5LIB=$CPAN_BASE/$i/$j/$k:$PERL5LIB
+          export PERL5LIB=$XCR_CPAN_BASE/$i/$j/$k:$PERL5LIB
       done
   done
 done
