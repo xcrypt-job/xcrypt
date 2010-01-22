2b8a45011c2f3d7f777abe85f7f24f407f588bb8
tasuku <tasuku@PC-6173>
2010-01-22 11:34:16 +0900

    (1) Modified CPAN installation scirpt to support installing in usr/local. (2) Removed conftest.c from Coro-5.21 because of licencing problem. (3) Using Recursive.pm as a CPAN library.

--- cpan/do-install.sh
+++ cpan/do-install.sh
@@ -7,14 +7,24 @@ fi
 PERLVER=5.8.5
 ARCH=x86_64-linux-thread-multi
 CPAN_BASE=$XCRYPT/lib/cpan
+for i in usr usr/local; do
+  for j in lib lib64; do
+      for k in perl5 perl5/site_perl; do
+          export PERL5LIB=$CPAN_BASE/$i/$j/$k:$PERL5LIB
+      done
+  done
+done
+
 CPAN_LIB=$CPAN_BASE/usr/lib/perl5
 CPAN_LIB64=$CPAN_BASE/usr/lib64/perl5
+CPAN_LLIB=$CPAN_BASE/usr/local/lib/perl5
+CPAN_LLIB64=$CPAN_BASE/usr/local/lib64/perl5
 
 export PERL5LIB=$CPAN_LIB:$CPAN_LIB/site_perl:$CPAN_LIB64:$CPAN_LIB64/site_perl:$PERL5LIB
 
 # /perl5/site_perl/$PERLVER/:$CPAN_DIR/usr/lib64/perl5/site_perl/$PERLVER/$ARCH/:$PERL5LIB
 
-LIBS="EV-3.9 Event-1.13 AnyEvent-5.24 common-sense-3.0 Guard-1.021 Coro-5.21"
+LIBS="File-Copy-Recursive-0.38 EV-3.9 Event-1.13 AnyEvent-5.24 common-sense-3.0 Guard-1.021 Coro-5.21-without-conftest"
 
 echo "Removing CPAN working directories."
 for i in $LIBS
@@ -26,9 +36,10 @@ done
 echo "Removing CPAN install directory."
 rm -rf $CPAN_BASE/usr
 
-echo "Start install."
+echo "Start installation."
 for i in $LIBS
 do
+  echo ">>> installing $i <<<"
   (cd $i && perl Makefile.PL && make DESTDIR=$CPAN_BASE install)
 done
   
