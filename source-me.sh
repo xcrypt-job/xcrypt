bf442aefb767230eacbc095117d7f9a29b6e1b54
abet <abet@localhost>
2010-01-22 13:26:16 +0900

    modify Xcrypt_manual along change of how to install Coro

--- source-me.sh
+++ source-me.sh
@@ -1,10 +1,10 @@
-export XCRYPT=/path/to/xcrypt
+#export XCRYPT=/path/to/xcrypt
 export XCRJOBSCHED="sh"
 export PERL5LIB=$XCRYPT/lib:$PERL5LIB
 export XCR_CPAN_BASE=$XCRYPT/lib/cpan
-for i in usr usr/local; do
-  for j in lib lib64; do
-      for k in perl5 perl5/site_perl; do
+for i in usr usr/local usr/local/share; do
+  for j in lib lib64 perl; do
+      for k in perl5 perl5/site_perl 5.10.0; do
           export PERL5LIB=$XCR_CPAN_BASE/$i/$j/$k:$PERL5LIB
       done
   done
