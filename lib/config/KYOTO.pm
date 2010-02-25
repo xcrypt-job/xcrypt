a8bd14d8d06bf6a0cbc5b7106ce7822905072a80
tasuku <tasuku@PC-6173>
2010-02-25 18:41:17 +0900

    Strictry defined the specification for jobscheduler configulation files

--- lib/config/KYOTO.pm
+++ lib/config/KYOTO.pm
@@ -1,19 +1,30 @@
 # Config file for Kyoto Fujitsu NQS
+use config_common;
+use File::Spec;
+
 $jsconfig::jobsched_config{"KYOTO"} = {
+    # commands
     qsub_command => "/thin/local/bin/qsub",
     qdel_command => "/usr/bin/qdel -K",
     qstat_command => "/thin/local/bin/qstat",
-    jobscript_queue => '# @$-q ',
-    jobscript_group => '# @$-g ',
-    jobscript_stdout => '# @$-o ',
-    jobscript_stderr => '# @$-e ',
-    jobscript_proc => '# @$-lP ',
-    jobscript_cpu => '# @$-lp ',
-    jobscript_memory => '# @$-lm ',
-    jobscript_stack => '# @$-ls ',
-    jobscript_verbose => '# @$-oi',
-    jobscript_verbose_node => '# @$-OI',
-    jobscript_workdir => '$QSUB_WORKDIR',
+    # standard options
+    jobscript_preamble => ['#!bin/sh'];
+    jobscript_workdir => sub { File::Spec->catfile('$QSUB_WORKDIR',$_[0]->{id}); }
+    jobscript_option_stdout => workdir_file_option ('# @$-o '),
+    jobscript_option_stderr => workdir_file_option ('# @$-e '),
+    jobscript_option_merge_output => boolean_option ('# @$-eo'),
+    jobscript_option_node => '# @$-lP ',
+    jobscript_option_cpu => '# @$-lp ',
+    jobscript_option_memory => '# @$-lm ',
+    jobscript_option_limit_time => '# @$-cp ',
+    jobscript_option_limit_cputime => '# @$-lT ',
+    jobscript_option_queue => '# @$-q ',
+    jobscript_option_group => '# @$-g ',
+    # non-standard options
+    jobscript_option_stack => '# @$-ls ',
+    jobscript_option_verbose => boolean_option ('# @$-oi'),
+    jobscript_option_verbose_node => boolean_option ('# @$-OI'),
+    # Extract from output messages
     extract_req_id_from_qsub_output => sub {
         my (@lines) = @_;
         if ($lines[0] =~ /([0-9]*)\.nqs/) {
