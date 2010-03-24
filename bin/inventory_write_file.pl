e79b93af3c2b76f3a022013d1a04726505e5637b
abet <abet@abet-laptop>
2010-03-24 14:43:12 +0900

    for removing bug

--- bin/inventory_write_file.pl
+++ bin/inventory_write_file.pl
@@ -52,6 +52,7 @@ sub release_lockdir {
         print $LOG "$JOBNAME\[$STATUS\]: release_lockdir called, but $_[0] not exists.\n";
     } else {
         until ($succ) {
+	    print "foo\n";
 	    $succ = rmdir ($_[0]);
         }
         print $LOG "$JOBNAME\[$STATUS\]: Successfully released lockdir.\n";
