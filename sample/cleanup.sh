10cb15493bb96f34ac93b763ff122460a53d9f50
tasuku <tasuku@PC-6173>
2010-05-14 07:47:45 +0900

    xcryptdel supports communication via file using xcrypt_comm.pm

--- sample/cleanup.sh
+++ sample/cleanup.sh
@@ -5,5 +5,4 @@ find . -name "bulk[0-9]*"| xargs rm -rf
 find . -name "job[0-9]*"| xargs rm -rf
 find . -name ".tmp"| xargs rm -rf
 find . -name "inv_watch"| xargs rm -rf
-find . -name "invwrite-sock.log"| xargs rm -f
-find . -name "invwrite-file.log"| xargs rm -f
+find . -name "invwrite.log"| xargs rm -f
