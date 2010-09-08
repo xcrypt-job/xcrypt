72563ec3f3ad4b351724397745dde18f8024d2ce
abet <abet@localhost>
2010-09-08 13:57:04 +0900

    add cpoption_searcher.pm

---
+++ lib/elapsed_time.pm
@@ -0,0 +1,45 @@
+package elapsed_time;
+
+use strict;
+use jobsched;
+use builtin;
+
+&add_key('max_time');
+
+sub new {
+    my $class = shift;
+    my $self = $class->NEXT::new(@_);
+    return bless $self, $class;
+}
+
+my $time_init = undef;
+my $time_now = undef;
+my $slp = 0;
+my $cycle = 5;
+sub start {
+    my $self = shift;
+
+    if (defined $self->{max_time}) {
+	Coro::async {
+	    &jobsched::wait_job_running ($self);
+	    $time_init = time();
+	    my $stat = 'running';
+	    until ($stat eq 'done') {
+		Coro::AnyEvent::sleep $cycle;
+		$stat = &jobsched::get_job_status ($self);
+		$time_now = time();
+		my $elapsed = $time_now - $time_init;
+		print $elapsed, "\n";
+		if ($self->{max_time} < $elapsed) {
+		    $self->invalidate();
+		    $stat = 'done';
+		}
+	    }
+	} $self;
+	Coro::AnyEvent::sleep $slp;
+    }
+
+    $self->NEXT::start();
+}
+
+1;
