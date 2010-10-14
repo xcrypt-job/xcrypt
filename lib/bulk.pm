54e4a9201853ec250648d93cb33d7f5a9e193277
abet <abet@localhost>
2010-10-14 20:26:35 +0900

    update bulk.pm

--- lib/bulk.pm
+++ lib/bulk.pm
@@ -6,11 +6,15 @@ use jobsched;
 use Cwd;
 use File::Basename;
 use File::Spec;
+use Time::HiRes;
+use Coro;
+use Coro::Channel;
+use common;
 use jsconfig;
 
 our %bulk = ();
 
-&add_key('bulkedjobs');
+&add_key('bulk_jobs', 'bulk_id', 'time');
 
 sub initialize {
     if ( $_[0] =~ /^CODE\(/ ) {
@@ -35,19 +39,48 @@ sub initialize {
 }
 
 sub bulk {
-    my $id = shift;
+    my $id   = shift;
     my @jobs = @_;
+    
     if ( %bulk == () ) {
         ### All jobs bulk ###
-        my %frame            = %{$jobs[0]};
-        $frame{id}         = "$id";
-        $frame{exe}        = '';
-        $frame{bulkedjobs} = \@jobs;
-        my $bulk_job = user->new(\%frame);
+        my %frame         = %{$jobs[0]};
+        $frame{id}        = "$id";
+        $frame{exe}       = '';
+        $frame{bulk_jobs} = \@jobs;
+        delete $frame{status};
+        my $bulk_job      = user->new(\%frame);
+        # Entry job's
+        &jobsched::entry_job_id ($bulk_job);
+        &jobsched::set_job_initialized($bulk_job);
+        &jobsched::set_job_prepared($bulk_job);
+        # stderr & stdout
+        $bulk_job->{'JS_stdout'} = "$bulk_job->{id}_stdout";
+        $bulk_job->{'JS_stderr'} = "$bulk_job->{id}_stderr";
+        # Job script related members
+        $bulk_job->{'jobscript_file'}     = "$bulk_job->{id}_$bulk_job->{env}->{sched}.sh";
+        $bulk_job->{'before_in_job_file'} = "$bulk_job->{id}_before_in_job.pl";
+        $bulk_job->{'after_in_job_file'}  = "$bulk_job->{id}_after_in_job.pl";
+        # return
         return ($bulk_job);
     } elsif ( exists $bulk{user_bulk} ) {
         ### user function bulk ###
         my @bulk_jobs = $bulk{user_bulk}->($id, @jobs);
+        foreach my $bulk_job (@bulk_jobs) {
+            delete $bulk_job->{status};
+            # Entry job's
+            &jobsched::entry_job_id ($bulk_job);
+            &jobsched::set_job_initialized($bulk_job);
+            &jobsched::set_job_prepared($bulk_job);
+            # stderr & stdout
+            $bulk_job->{'JS_stdout'} = "$bulk_job->{id}_stdout";
+            $bulk_job->{'JS_stderr'} = "$bulk_job->{id}_stderr";
+            # Job script related members
+            $bulk_job->{'jobscript_file'} = "$bulk_job->{id}_$bulk_job->{env}->{sched}.sh";
+            $bulk_job->{'before_in_job_file'} = "$bulk_job->{id}_before_in_job.pl";
+            $bulk_job->{'after_in_job_file'} = "$bulk_job->{id}_after_in_job.pl";
+        }
+        # return
         return (@bulk_jobs);
     } else {
         ### designated unit bulk ###
@@ -58,7 +91,7 @@ sub bulk {
         my @bulk_job  = ();
         my @bulk_jobs = ();
         my $time_cnt  = 0;
-        my %frame      = %{$jobs[0]};
+        my %frame     = %{$jobs[0]};
         for ( my $i = 0; $i <= $jobs_max; $i++ ) {
             my $job = shift @jobs;
             # check job plan execute time
@@ -66,7 +99,7 @@ sub bulk {
                 die "Bulk Plan Time Error\n";
             }
             # check bulk job plan maximum execute time and queue name
-            if ( ((!exists $bulk{max_time}) or $bulk{max_time} >= ($time_cnt + $job->{time})) and $frame{queue} eq $job->{queue} ) {
+            if ( ((!exists $bulk{max_time}) or $bulk{max_time} >= ($time_cnt + $job->{time})) and $frame{JS_queue} eq $job->{JS_queue} ) {
                 $time_cnt += $job->{time};
                 push (@bulk_job, $job);
                 $job_cnt++;
@@ -78,11 +111,12 @@ sub bulk {
             } else {
                 # register bulk job
                 $bulk_cnt++;
-                $frame{id}         = "${id}_${bulk_cnt}";
-                $frame{exe}        = '';
-                my @bulk_job_tmp     = @bulk_job;
-                $frame{bulkedjobs} = \@bulk_job_tmp;
-                my %frame_tmp        = %frame;
+                $frame{id}        = "${id}_${bulk_cnt}";
+                $frame{exe}       = '';
+                my @bulk_job_tmp  = @bulk_job;
+                $frame{bulk_jobs} = \@bulk_job_tmp;
+                delete $frame{status};
+                my %frame_tmp     = %frame;
                 push (@bulk_jobs, (user->new(\%frame_tmp)));
                 # initial setting
                 %frame    = %{$job};
@@ -95,10 +129,11 @@ sub bulk {
             if ( ((exists $bulk{max_num}) and $bulk{max_num} <= $job_cnt) or $i >= $jobs_max ) {
                 # register bulk job
                 $bulk_cnt++;
-                $frame{id}         = "${id}_${bulk_cnt}";
-                $frame{exe}        = '';
-                my @bulk_job_tmp     = @bulk_job;
-                $frame{bulkedjobs} = \@bulk_job_tmp;
+                $frame{id}        = "${id}_${bulk_cnt}";
+                $frame{exe}       = '';
+                my @bulk_job_tmp  = @bulk_job;
+                $frame{bulk_jobs} = \@bulk_job_tmp;
+                delete $frame{status};
                 my %frame_tmp        = %frame;
                 push (@bulk_jobs, (user->new(\%frame_tmp)));
                 # initial setting
@@ -107,6 +142,20 @@ sub bulk {
                 $time_cnt = 0;
             }
         }
+        foreach my $bulk_job (@bulk_jobs) {
+            # Entry job's
+            &jobsched::entry_job_id ($bulk_job);
+            &jobsched::set_job_initialized($bulk_job);
+            &jobsched::set_job_prepared($bulk_job);
+            # stderr & stdout
+            $bulk_job->{'JS_stdout'} = "$bulk_job->{id}_stdout";
+            $bulk_job->{'JS_stderr'} = "$bulk_job->{id}_stderr";
+            # Job script related members
+            $bulk_job->{'jobscript_file'} = "$bulk_job->{id}_$bulk_job->{env}->{sched}.sh";
+            $bulk_job->{'before_in_job_file'} = "$bulk_job->{id}_before_in_job.pl";
+            $bulk_job->{'after_in_job_file'} = "$bulk_job->{id}_after_in_job.pl";
+        }
+        # return
         return (@bulk_jobs);
     }
 }
@@ -127,39 +176,129 @@ sub start {
         if ( $stat eq 'done' ) {
             print "Skipping " . $self->{id} . " because already $stat.\n";
         } else {
+            $self->qsub_make($self);
             $self->{request_id} = &qsub($self);
+            foreach my $sub_self (@{$self->{bulk_jobs}}) {
+                $sub_self->{request_id} = $self->{request_id};
+            }
         }
+    }
+}
+
+sub before {}
+sub after {}
+
+# Create a job script from information of the job object.
+# The result is stored in @{$self->{jobscript_header}} and @{$self->{jobscript_body}}
+sub make_jobscript {
+    my $self = shift;
+    my %cfg = %{$jsconfig::jobsched_config{$self->{env}->{sched}}};
+    # Chdir to the job's working directory
+    my $wkdir_str = File::Spec->catfile($self->{env}->{wd}, $self->{workdir});
+    # make_jobscript_header
+    $self->make_jobscript_header($self);
+    if (defined ($cfg{jobscript_workdir})) {
+        my $js_wkdir = $cfg{jobscript_workdir};
+        unless (ref ($js_wkdir)) {
+            $wkdir_str = $js_wkdir;
+        } elsif (ref ($js_wkdir) eq 'CODE') {
+            $wkdir_str = &$js_wkdir($self);
+        } else {
+            warn "Error in config file $self->{env}->{sched}: jobscript_workdir is neither scalar nor CODE."
+        }
+    }
+    unless ($self->{rhost} eq '') {
+        $wkdir_str = File::Spec->catfile( $self->{rwd}, $wkdir_str );
+    }
+    
+    # make_jobscript_body
+    foreach my $sub_self (@{$self->{bulk_jobs}}) {
+        $sub_self->make_jobscript_header($sub_self);
+        $sub_self->make_jobscript_body($sub_self);
+        push (@{$self->{jobscript_body}}, @{$sub_self->{jobscript_body}});
+    }
+    # Set the job's status to "running"
+    unshift (@{$self->{jobscript_body}},  jobsched::inventory_write_cmdline($self, 'running'). " || exit 1");
+    unshift (@{$self->{jobscript_body}}, "sleep 1"); # running が早すぎて queued がなかなか勝てないため
+    # Set the job's status to "done" (should set to "aborted" when failed?)
+    push (@{$self->{jobscript_body}}, "cd $wkdir_str");
+    push (@{$self->{jobscript_body}}, jobsched::inventory_write_cmdline($self, 'done'). " || exit 1");
+}
+
+# Submit a job specified by a jop object ($self) by executing "qsub"
+# after creating a job script file and a string of command-line option.
+sub qsub_make {
+    my $self = shift;
+    my $sched = $self->{env}->{sched};
+    unless (defined $jsconfig::jobsched_config{$sched}) {
+        die "$sched.pm doesn't exist in lib/config";
+    }
+    my %cfg = %{$jsconfig::jobsched_config{$sched}};
+    # Create JobScript & qsub options
+    $self->make_jobscript();
+    $self->make_qsub_options();
+    if (defined $cfg{modify}) {
+        &{$cfg{modify}} ($self);
+    }
+    #$self->make_before_in_job_script();
+    #$self->make_after_in_job_script();
+    $self->update_all_script_files();
+    foreach my $sub_self (@{$self->{bulk_jobs}}) {
+        $sub_self->make_before_in_job_script();
+        $sub_self->make_after_in_job_script();
+        $sub_self->update_all_script_files();
     }
 }
 
 sub qsub {
     my $self = shift;
-    # for inventory_write_file
-    my $inventory_path = $xcropt::options{inventory_path};
-    my $LOCKDIR = File::Spec->rel2abs(File::Spec->catfile($inventory_path, 'inventory_lock'));
-    my $REQUESTFILE = File::Spec->rel2abs(File::Spec->catfile($inventory_path, 'inventory_req'));
-    my $ACKFILE = File::Spec->rel2abs(File::Spec->catfile($inventory_path, 'inventory_ack'));
-    my $REQUEST_TMPFILE = $REQUESTFILE . '.tmp';
-    my $ACK_TMPFILE = $ACKFILE . '.tmp';
-    rmdir $LOCKDIR;
-    unlink $REQUEST_TMPFILE, $REQUESTFILE, $ACK_TMPFILE, $ACKFILE;
-    # print job script file
-    $self = &jobsched::print_job_scriptfile ($self);
-    foreach my $subself (@{$self->{bulkedjobs}}) {
-        &jobsched::inventory_write($subself, 'prepared');
-        &jobsched::print_job_scriptfile ($subself);
-    }
+    my $scriptfile = $self->workdir_member_file('jobscript_file');
+    my $qsub_options = join(' ', @{$self->{qsub_options}});
     # Set job's status "submitted"
-    &jobsched::inventory_write($self, "submitted");
-    foreach my $subself (@{$self->{bulkedjobs}}) {
-        &jobsched::inventory_write($subself, "submitted");
+    &jobsched::set_job_submitted($self);
+    foreach my $sub_self (@{$self->{bulk_jobs}}) {
+        #&jobsched::set_job_initialized($sub_self);
+        #&jobsched::set_job_prepared($sub_self);
+        &jobsched::set_job_submitted($sub_self);
     }
-    # qsub job script file
-    return &jobsched::qsub_job_scriptfile ($self);
+    
+    my $sched = $self->{env}->{sched};
+    my %cfg = %{$jsconfig::jobsched_config{$sched}};
+    my $qsub_command = $cfg{qsub_command};
+    unless ( defined $qsub_command ) {
+        die "qsub_command is not defined in $sched.pm";
+    }
+    
+    my $flag;
+    $flag = common::cmd_executable ($qsub_command, $self->{env});
+    if ($flag) {
+        # Execute qsub command
+        my $cmdline = "$qsub_command $qsub_options $scriptfile";
+        if ($xcropt::options{verbose} >= 2) { print "$cmdline\n"; }
+        my @qsub_output = &xcr_qx($self->{env}, "$cmdline", $self->{workdir});
+        if ( @qsub_output == 0 ) { die "qsub command failed."; }
+        # Get request ID from qsub's output
+        my $req_id;
+        if ( defined ($cfg{extract_req_id_from_qsub_output}) ) {
+            unless ( ref $cfg{extract_req_id_from_qsub_output} eq 'CODE' ) {
+                die "Error in $sched.pm: extract_req_id_from_qsub_output must be a function";
+            }
+            $req_id = &{$cfg{extract_req_id_from_qsub_output}} (@qsub_output);
+        } else { # default extractor
+            $req_id = ($qsub_output[0] =~ /([0-9]+)/) ? $1 : -1;
+        }
+        if ( $req_id < 0 ) { die "Can't extract request ID from qsub output." }
+        # Remember request ID
+        $self->{request_id} = $req_id;
+        # Set job's status "queued"
+        &jobsched::set_job_queued($self);
+        foreach my $sub_self (@{$self->{bulk_jobs}}) {
+            $sub_self->{request_id} = $req_id;
+            &jobsched::set_job_queued($sub_self);
+        }
+        return $req_id;
+    } else {
+        die "$qsub_command is not executable";
+    }
 }
-
-sub before {}
-
-sub after {}
-
 1;
