601a73442a79e614efc655b5ae8477304cb42985
abet <abet@localhost>
2010-10-27 09:57:34 +0900

    implement xcrypt --scratch

--- bin/script/invalidateall.xcr.pl
+++ bin/script/invalidateall.xcr.pl
@@ -10,8 +10,7 @@ sub before {local ($self, @VALUE) = @_; if ($self->{before}) {&{$self->{before}}
 sub start  {my $self = shift;$self->SUPER::start();}
 sub after  {local ($self, @VALUE) = @_; if ($self->{after} ) {&{$self->{after}}($self, @VALUE)};}
 # Up to here Xcrypt's header.  From here your script.
-&jobsched::read_log();
-if ($xcropt::options{print_log}) { &jobsched::print_log(); }
+unless ($xcropt::options{scratch}) { &jobsched::read_log(); }
 &jobsched::invoke_abort_check();
 &jobsched::invoke_left_message_check();
 $builtin::env_d = &add_host({"host" => $xcropt::options{host}, "wd" => $xcropt::options{wd}}, "sched" => $xcropt::options{sched}, "xd" => $xcropt::options{xd}, "p5l" => $xcropt::options{p5l});
