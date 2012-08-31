717868053261bc5f45cafaec1748d361ad45b247
tasuku <tasuku@localhost>
2012-08-31 10:54:54 +0900

    Deleted an obsoleted script file.

--- source-me.sh
+++
@@ -1,6 +0,0 @@
-if [ "x$XCRYPT" = "x" ]; then
-    export XCRYPT=$HOME/xcrypt
-fi
-export PATH=$XCRYPT/bin:$PATH
-export XCR_CPAN_BASE=$XCRYPT/lib/cpan
-export PERL5LIB=$XCRYPT/lib:$XCRYPT/lib/algo/lib:$XCR_CPAN_BASE:$PERL5LIB
